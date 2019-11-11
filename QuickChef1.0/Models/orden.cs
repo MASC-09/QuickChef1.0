88f7ebeaebf9bbdc4ee567aa982eb537d6fb28ad
badatnick <56774760+badatnick@users.noreply.github.com>
2019-11-10 20:25:08 -0600

    Maes se hizo la pate de orden

---
+++ QuickChef1.0/Models/orden.cs
@@ -0,0 +1,30 @@
+﻿namespace QuickChef1._0.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Data.Entity;
+
+
+    public class orden
+    {
+        [Key]
+        public int? relativo { get; set; }
+
+        public int? Mesa { get; set; }
+
+        
+        public string platilloID { get; set; }
+
+        public int? cantidad { get; set; }
+
+        
+    }
+
+    public class ordenContext : DbContext
+    {
+        public DbSet<orden> orden { get; set; }
+    }
+}
