e67615016f4533e4c6d2fd9b8c8bbc26ecd04a0a
FriendshipBreaker <FriendshipBreaker@users.noreply.github.com>
2019-11-24 18:29:02 -0600

    orden/encuesta

---
+++ QuickChef1.0/Controllers/AdminEncuestasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuickChef1._0.Models;
+
+namespace QuickChef1._0.Controllers
+{
+    public class AdminEncuestasController : Controller
+    {
+        public ActionResult LlenarEncuesta()
+        {
+            return View();
+        }
+
+        private EncuestaContext db = new EncuestaContext();
+
+        // GET: AdminEncuestas
+        public ActionResult Index()
+        {
+            return View(db.Encuesta.ToList());
+        }
+
+        // GET: AdminEncuestas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Encuesta encuesta = db.Encuesta.Find(id);
+            if (encuesta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(encuesta);
+        }
+
+        // GET: AdminEncuestas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminEncuestas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EncuestaID,NumMesa,Puntuacion")] Encuesta encuesta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Encuesta.Add(encuesta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(encuesta);
+        }
+
+        // GET: AdminEncuestas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Encuesta encuesta = db.Encuesta.Find(id);
+            if (encuesta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(encuesta);
+        }
+
+        // POST: AdminEncuestas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EncuestaID,NumMesa,Puntuacion")] Encuesta encuesta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(encuesta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(encuesta);
+        }
+
+        // GET: AdminEncuestas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Encuesta encuesta = db.Encuesta.Find(id);
+            if (encuesta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(encuesta);
+        }
+
+        // POST: AdminEncuestas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Encuesta encuesta = db.Encuesta.Find(id);
+            db.Encuesta.Remove(encuesta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QuickChef1.0/Controllers/LlenarEncuestaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuickChef1._0.Models;
+
+namespace QuickChef1._0.Controllers
+{
+    public class LlenarEncuestaController : Controller
+    {
+        private EncuestaContext db = new EncuestaContext();
+
+        // GET: LlenarEncuesta
+        public ActionResult Index()
+        {
+            return View(db.Encuesta.ToList());
+        }
+
+        // GET: LlenarEncuesta/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Encuesta encuesta = db.Encuesta.Find(id);
+            if (encuesta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(encuesta);
+        }
+
+        // GET: LlenarEncuesta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LlenarEncuesta/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EncuestaID,NumMesa,Puntuacion")] Encuesta encuesta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Encuesta.Add(encuesta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(encuesta);
+        }
+
+        // GET: LlenarEncuesta/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Encuesta encuesta = db.Encuesta.Find(id);
+            if (encuesta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(encuesta);
+        }
+
+        // POST: LlenarEncuesta/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EncuestaID,NumMesa,Puntuacion")] Encuesta encuesta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(encuesta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(encuesta);
+        }
+
+        // GET: LlenarEncuesta/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Encuesta encuesta = db.Encuesta.Find(id);
+            if (encuesta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(encuesta);
+        }
+
+        // POST: LlenarEncuesta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Encuesta encuesta = db.Encuesta.Find(id);
+            db.Encuesta.Remove(encuesta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
