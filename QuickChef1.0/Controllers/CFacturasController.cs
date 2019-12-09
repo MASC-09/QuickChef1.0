9e819ba512e2b8683f2cc365d629d4465f9efb90
FriendshipBreaker <FriendshipBreaker@users.noreply.github.com>
2019-12-08 23:08:44 -0600

    factura

---
+++ QuickChef1.0/Controllers/CFacturasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuickChef1._0.Models;
+
+namespace QuickChef1._0.Controllers
+{
+    public class CFacturasController : Controller
+    {
+        private FacturaContext db = new FacturaContext();
+
+        // GET: CFacturas
+        public ActionResult Index()
+        {
+            return View(db.Factura.ToList());
+        }
+
+        // GET: CFacturas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(factura);
+        }
+
+        // GET: CFacturas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CFacturas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "relativo,Mesa,platilloID,cantidad,precio,EstadoOrden")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Factura.Add(factura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(factura);
+        }
+
+        // GET: CFacturas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(factura);
+        }
+
+        // POST: CFacturas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "relativo,Mesa,platilloID,cantidad,precio,EstadoOrden")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(factura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(factura);
+        }
+
+        // GET: CFacturas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Factura factura = db.Factura.Find(id);
+            if (factura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(factura);
+        }
+
+        // POST: CFacturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Factura factura = db.Factura.Find(id);
+            db.Factura.Remove(factura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QuickChef1.0/Models/Factura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QuickChef1._0.Models
+{
+    public class Factura
+    {
+        [Key]
+        public int? relativo { get; set; }
+
+        public int? Mesa { get; set; }
+
+
+        public string platilloID { get; set; }
+
+        public int? cantidad { get; set; }
+
+        public int? precio { get; set; }
+        public string EstadoOrden { get; set; }
+    }
+
+    public class FacturaContext : DbContext
+    {
+        public DbSet<Factura> Factura { get; set; }
+    }
+}
